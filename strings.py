7a7c5e7de1ad114e63bf0021db97177ea5b9a767
PeterMwitiDev <mwitipet@gmal.com>
2024-09-04 03:50:32 +0300

    first pythonDSA commit added strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+
+#multiple screan
+measage = """ 
+Never gonna be sad
+Never gonna be broke
+"""
+print(measage)
+
+fName = "Peter"
+sName = "Mwiti"
+
+fullName = fName +" " + sName
+print(fullName)
+
+for letter in fullName:
+    print (letter)
+
+print(len(fullName))    
